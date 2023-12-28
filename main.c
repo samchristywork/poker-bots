fca2a66508d0d5184c2d2fc5bc9a9bcbe8975d49
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-12-27 21:37:40 -0600

    feat: Add basic data
    This data describes game state. I think I will refactor this into a
    struct later, but for now they can be global variables.

--- main.c
+++ main.c
@@ -19,5 +19,16 @@ struct rank {
   int value;
 };
 
+struct card community[5];
+struct player players[5];
+struct card deck[52];
+int deck_position = 0;
+int small_blind = 1;
+int big_blind = 2;
+int pot = 0;
+int community_shown = 0;
+int hand_shown = 2;
+int starting_player = 0;
+
 int main() {
 }
