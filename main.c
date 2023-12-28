c99132eee0e9647b053e26a3fee740b77cad8cf7
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-12-27 21:38:20 -0600

    feat: Add function to initialize the deck
    This function initialises a 52 card deck with 13 cards in each suit.

--- main.c
+++ main.c
@@ -30,5 +30,29 @@ int community_shown = 0;
 int hand_shown = 2;
 int starting_player = 0;
 
+void init_deck() {
+  int i = 0;
+  for (int suit = 0; suit < 4; suit++) {
+    for (int value = 2; value < 15; value++) {
+      deck[i].value = value;
+      switch (suit) {
+      case 0:
+        deck[i].suit = 'H';
+        break;
+      case 1:
+        deck[i].suit = 'D';
+        break;
+      case 2:
+        deck[i].suit = 'C';
+        break;
+      case 3:
+        deck[i].suit = 'S';
+        break;
+      }
+      i++;
+    }
+  }
+}
+
 int main() {
 }
